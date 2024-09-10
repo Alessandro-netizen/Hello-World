ae6a59a9a777236ebd96f87b4ba1eb830da6470f
Alessandro-netizen <alessandrom12@nycstudents.net>
2024-09-10 18:28:41 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,10 +3,13 @@ public class Main
 
     public static void main(String[] args)
     {
-        System.out.println("Hello world.");
-
-        System.out.print("Hello World");
-        
-
+        double x = 2.3;
+        int y = 4;
+        String message = "string";
+        boolean z = false;
+        y = y + x;
+        System.out.print(y + z);
+        System.out.print(" " + message);
     }
 }
+        
